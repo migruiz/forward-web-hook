b7075fbe6888cfaa4ef6d0b22aaba293ade4e803
Miguel <mig.ruiz@gmail.com>
2019-05-11 04:41:11 +0100

    using Env Var

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@
 const SmeeClient = require('smee-client')
 
 const smee = new SmeeClient({
-  source: 'https://smee.io/e7b3jV8eXIcw161',
-  target: 'http://sonia:11616c4700d6e54c76757129c1b05e8c27@192.168.1.217:8008/job/musterGIT/build?token=34DEB6763DDEA3CF',
+  source: process.env.SOURCE_URL,
+  target: process.env.TARGET_URL,
   logger: console
 })
 
