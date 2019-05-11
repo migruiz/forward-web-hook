a7ac65f85f3dbd56878bc05f269cf48c53ebbd4c
Miguel <mig.ruiz@gmail.com>
2019-05-11 04:36:06 +0100

    testing trigger

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const SmeeClient = require('smee-client')
 
 const smee = new SmeeClient({
   source: 'https://smee.io/e7b3jV8eXIcw161',
-  target: 'http://localhost:3000/events',
+  target: 'http://sonia:11616c4700d6e54c76757129c1b05e8c27@192.168.1.217:8008/job/musterGIT/build?token=34DEB6763DDEA3CF',
   logger: console
 })
 
